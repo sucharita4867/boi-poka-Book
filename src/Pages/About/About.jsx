1e092418789be155151644489ab20424fd249a93
sucharita4867 <sucharitasardar7@gmail.com>
2026-01-20 21:48:04 +0530

    delete boipoka about book icon

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className="max-w-4xl mx-auto px-6 py-12">
       <h1 className="text-4xl font-bold text-center mb-6 text-indigo-600">
-        About Boi Poka 📚
+        About Boi Poka
       </h1>
 
       <p className="text-lg text-gray-700 leading-relaxed mb-6">
